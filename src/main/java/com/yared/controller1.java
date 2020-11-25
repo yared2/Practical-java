cf2be67349a6216a5084ac48b245da5028386a99
yared2 <yared555@gmail.com>
2020-11-25 12:38:32 -0600

    Add log to file
    change system.out.println to spring

--- src/main/java/com/yared/controller1.java
+++ src/main/java/com/yared/controller1.java
@@ -1,14 +1,20 @@
 package com.yared;
 
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.GetMapping;
 
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class controller1 {
+	
+	private Logger logger = LoggerFactory.getLogger(controller1.class);
 
 	@GetMapping("/hello-world")
 	public String helloWorld() {
+		logger.info("hello -world logger");
 		return "HelloWorld";
 	}
 
